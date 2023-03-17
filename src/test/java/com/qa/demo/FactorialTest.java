8b920ee508093db0d5c52a22490b0fd68e49c91c
Admin <Admin@192.168.1.100>
2023-03-17 13:03:35 +0000

    Added challenge tests

--- src/test/java/com/qa/demo/FactorialTest.java
+++ src/test/java/com/qa/demo/FactorialTest.java
@@ -16,4 +16,24 @@ public class FactorialTest {
 	void testNoneFactorial() {
 		assertEquals("NONE", this.fact.reverseFactorial(150));
 	}
+
+	@Test
+	void challengeInput1() {
+		assertEquals("3628800 = 10!", this.fact.reverseFactorial(3628800));
+	}
+
+	@Test
+	void challengeInput2() {
+		assertEquals("479001600 = 12!", this.fact.reverseFactorial(479001600));
+	}
+
+	@Test
+	void challengeInput3() {
+		assertEquals("6 = 3!", this.fact.reverseFactorial(6));
+	}
+
+	@Test
+	void challengeInput4() {
+		assertEquals("NONE", this.fact.reverseFactorial(18));
+	}
 }
