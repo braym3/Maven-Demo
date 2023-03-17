69a371b8e0700e62627c5af2960da58a141d6bb1
Admin <Admin@192.168.1.100>
2023-03-17 14:34:13 +0000

    Updated list places method to fix teens

--- src/main/java/com/qa/demo/dogcompetition/DogCompetition.java
+++ src/main/java/com/qa/demo/dogcompetition/DogCompetition.java
@@ -8,17 +8,19 @@ public class DogCompetition {
 		for (int i = 1; i <= 100; i++) {
 			if (i != place) {
 				lastDigit = i % 10;
-				// if ends in 1 - add "st"
-				if (lastDigit == 1) {
+				// if ends in 1 & not 11- add "st"
+				if (lastDigit == 1 && i != 11) {
 					s += i + "st";
 				}
-				// if ends in 2 - add "nd"
-				else if (lastDigit == 2) {
+				// if ends in 2 & not 12 - add "nd"
+				else if (lastDigit == 2 && i != 12) {
 					s += i + "nd";
 				}
-				// if ends in 3 - add "rd"
-				else if (lastDigit == 3) {
+				// if ends in 3 & not 13 - add "rd"
+				else if (lastDigit == 3 && i != 13) {
 					s += i + "rd";
+				} else {
+					s += i + "th";
 				}
 				// if not 100th number AND not on 99 and placed 100th - add comma space
 				if (i != 100 && !(i == 99 && place == 100)) {
